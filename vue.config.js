61c036dd9d355b1b11789b04176edbd1d5420ad9
愿世界被温柔相待 <1561808630@qq.com>
2019-11-20 18:39:09 +0800

    地址修改

--- vue.config.js
+++ vue.config.js
@@ -11,21 +11,22 @@ module.exports = {
         hotOnly: true, //是否开启热更新
         proxy: {
             '/api': {
-                // target: 'http://192.168.10.25:9998', //本地API服务器的地址--崔
-                //target: 'http://25hc897890.zicp.vip', //本地API服务器的地址--谢
+                // target: 'http://192.168.10.47:9998', //崔
+                // target: 'http://192.168.10.46:9998', //徐
+                // target: 'http://192.168.10.49:9998', //阳
                 target: 'http://112.125.25.93:9998', //服务器API服务器的地址
                 changeOrigin: true,
                 pathRewrite: {
                     '^/api': ''
                 }
-            }, '/index': {
-                target: 'http://localhost:8863', //本地API服务器的地址--崔
-                //target: 'http://172.31.9.138:9998', //本地API服务器的地址--谢
-                // target: 'http://112.125.25.93:9998', //服务器API服务器的地址
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/index': ''
-                }
+            // }, '/index': {
+            //     target: 'http://localhost:8863', //本地API服务器的地址--崔
+            //     //target: 'http://172.31.9.138:9998', //本地API服务器的地址--谢
+            //     // target: 'http://112.125.25.93:9998', //服务器API服务器的地址
+            //     changeOrigin: true,
+            //     pathRewrite: {
+            //         '^/index': ''
+            //     }
             }
         }
     }
